02c9761b26755b4574758ab8e59bfbb0aff69372
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2015-09-29 15:59:00 +0200

    Added build process and simple function for extrema

---
+++ deps/src/Persistence1D/persistence1d.cpp
@@ -0,0 +1,31 @@
+#include "persistence1d.hpp"
+#include <cstddef>
+#include <vector>
+#include <algorithm>
+
+extern "C" {
+  void find_extrema(const float* v, std::size_t n,
+                    int** minIndices, std::size_t* nmin,
+                    int** maxIndices, std::size_t* nmax,
+                    const float threshold = 0);
+}
+
+void find_extrema(const float* v, std::size_t n,
+                  int** minIndices, std::size_t* nmin,
+                  int** maxIndices, std::size_t* nmax,
+                  const float threshold)
+{
+  p1d::Persistence1D p;
+  std::vector<float> vv(n);
+  std::copy(v, v+n, std::begin(vv));
+  if(p.RunPersistence(vv)){
+    std::vector<int> min_v, max_v;
+    p.GetExtremaIndices(min_v, max_v, threshold, true);
+    *nmin = min_v.size();
+    *nmax = max_v.size();
+    *minIndices = new int[*nmin];
+    *maxIndices = new int[*nmax];
+    std::copy(std::begin(min_v), std::end(min_v), *minIndices);
+    std::copy(std::begin(max_v), std::end(max_v), *maxIndices);
+  }
+}
